ce0c83ed97e4f3158f8d3b9f2a3e44adae615e96
Murad Masimli <murad.mesimli.99@gmail.com>
2023-09-04 03:33:05 +0400

    fix: unused state

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,10 @@
-import { useSite } from "../context/AppProvider"
+
 import GameTable from "./GameTable"
 import TicTacToe from "./TicTacToe"
 
 const Game = () => {
 
-  const {winner} = useSite()
+  
   return (
     <section className="bg-white w-5/6 h-5/6 rounded-xl  flex ">
 
